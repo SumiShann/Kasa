b68d39d0f5237850ba324d1904a1dfbbc3520547
Violaine Barral <violainebarral@yahoo.fr>
2023-04-11 16:39:31 +0200

    animations button and dropdown collapse done

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import down from '../assets/arrow_down.svg'
 import '../styles/css/Collapse.css'
 
 function Collapse({title, content}){
     const [isOpen, setIsOpen] = useState(false)
+    const [render, setRender] = useState(false)
+    useEffect(() => {
+        if (isOpen) {
+            setTimeout(() => {
+                setRender(true)
+            }, 10)
+        } else {
+            setRender(false)
+        }
+    }, [isOpen])
     return isOpen ? (
         <section className='collapse'>
             <div className='collapse-heading'>
                 <h2>{title}</h2>
-                <button className='btn' onClick={() => setIsOpen(false)}><img src={down}/></button>
+                <button className={`btn ${isOpen ? 'up' : null}`} onClick={() => setIsOpen(false)}><img src={down}/></button>
             </div>
-            <div className='collapse-desc'>
+            <div className={`collapse-desc ${render ? 'shown' : null}`}>
                 {Array.isArray(content) ?
                     <ul className='content'>
                         {content.map((item, index) =>
@@ -24,7 +34,7 @@ function Collapse({title, content}){
         <section className='collapse'>
             <div className='collapse-heading'>
                 <h2>{title}</h2>
-                <button className='btn' onClick={() => setIsOpen(true)}><img src={down}/></button>
+                <button className={`btn ${isOpen ? 'up' : null}`} onClick={() => setIsOpen(true)}><img src={down}/></button>
             </div>
         </section>
     )
