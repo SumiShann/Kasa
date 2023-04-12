d3755142f9005d3847df7010006fb2c8a5944961
Violaine Barral <violainebarral@yahoo.fr>
2023-04-12 16:07:04 +0200

    deleting useless files

--- src/router.js
+++ src/router.js
@@ -5,12 +5,14 @@ import Accommodation from './pages/Accommodation';
 import ErrorBoundary from './pages/ErrorBoundary';
 
 function Router(){
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/accommodation/:id" element={<Accommodation />} />
-        <Route path="/*" element={<ErrorBoundary />}/>
-    </Routes>
+    return (
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/accommodation/:id" element={<Accommodation />} />
+            <Route path="/*" element={<ErrorBoundary />}/>
+        </Routes>
+    )
 }
 
 export default Router
